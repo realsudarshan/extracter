fe579319e08b4771a57bf9f4e9a1027bbccf47db
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2025-12-02 19:52:30 +0545

    fix: worflow refined

--- inngest/agent.ts
+++ inngest/agent.ts
@@ -1,25 +1,25 @@
-import {createNetwork,getDefaultRoutingAgent,gemini} from "@inngest/agent-kit"
+import { createNetwork, getDefaultRoutingAgent, gemini } from "@inngest/agent-kit"
 import { createServer } from "http"
 import { inngest } from "./client"
 import events from "./constants"
 import { databaseAgent } from "./agents/databaseAgent"
 import { receiptScanningAgent } from "./agents/receiptScanningAgent"
-const agentNetwork=createNetwork({
-    name:"Agent Team",
-    agents:[
-        databaseAgent,receiptScanningAgent
+const agentNetwork = createNetwork({
+    name: "Agent Team",
+    agents: [
+        databaseAgent, receiptScanningAgent
     ],
-    defaultModel:gemini({
-        model:"gemini-2.0-flash-lite",
-        defaultParameters:{
-            generationConfig:{
-                maxOutputTokens:1000,
+    defaultModel: gemini({
+        model: "gemini-2.0-flash-lite",
+        defaultParameters: {
+            generationConfig: {
+                maxOutputTokens: 1000,
             }
         }
     }),
-    defaultRouter:({network})=>{
-        const savedToDatabase=network.state.kv.get("saved-to-database")
-        if(savedToDatabase!==undefined){
+    defaultRouter: ({ network }) => {
+        const savedToDatabase = network.state.kv.get("saved-to-database")
+        if (savedToDatabase !== undefined) {
             //terminate the agent process if data has been saved to database
             return undefined
         }
@@ -28,21 +28,21 @@ const agentNetwork=createNetwork({
 
 })
 
-export const server=createServer({
-    agents:[databaseAgent,receiptScanningAgent],
-    network:[agentNetwork]
+export const server = createServer({
+    agents: [databaseAgent, receiptScanningAgent],
+    network: [agentNetwork]
 });
-export const extractAndSavePDF=inngest.createFunction(
-    {id:"Extract PDF and Save in Database"},
-{event:events.EXTRACT_DATA_FROM_PDF_AND_SAVE_TO_DATABASE},
-async({event}:any)=>{
-    console.log("📩 Received Inngest event:", event);
-    const result=await agentNetwork.run(
-        `Extract the key data from this pdf:${event.data.url}.
+export const extractAndSavePDF = inngest.createFunction(
+    { id: "Extract PDF and Save in Database" },
+    { event: events.EXTRACT_DATA_FROM_PDF_AND_SAVE_TO_DATABASE },
+    async ({ event }: any) => {
+        console.log("📩 Received Inngest event:", event);
+        const result = await agentNetwork.run(
+            `Extract the key data from this pdf:${event.data.url}.
         Once the data is extracted,save it to the database using the receiptId:${event.data.receiptId}.
         Once,the recipt is sucessfully saved to database you can terminate the agent process.Start with supervisor agent`
-    )
-    return result.state.kv.get("recipt") 
+        )
+        return result.state.kv.get("recipt")
 
-},
+    },
 )
