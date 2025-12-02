c797565852a72591a3720a7c8f6eb715836eac2a
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2025-12-02 21:56:25 +0545

    fix: inngest event key added

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,2 +1,5 @@
-import {Inngest} from "inngest"
-export const inngest=new Inngest({id:"reciept-tracker"})
+import { Inngest } from "inngest"
+export const inngest = new Inngest({
+    id: "reciept-tracker",
+    eventKey: process.env.INNGEST_EVENT_KEY!
+})
